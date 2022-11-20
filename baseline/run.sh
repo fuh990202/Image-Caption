7a756b6187f33436040b7e75af8253a8137203a0
Albert Zhu <albert.zhu@mail.utoronto.ca>
2022-11-20 18:26:34 -0500

    baseline model can create the caption with run.sh

---
+++ baseline/run.sh
@@ -0,0 +1,4 @@
+MODEL_PATH='./checkpoint_wordmap/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar'
+WORD_MAP_PATH='./checkpoint_wordmap/WORDMAP_coco_5_cap_per_img_5_min_word_freq.json'
+
+python3 ./app/caption.py --img='./image/image1.jpg' --model=$MODEL_PATH --word_map=$WORD_MAP_PATH --beam_size=5
