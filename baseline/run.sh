96bb130a50fce68ef5decc555270a489c83530f8
Albert Zhu <albert.zhu@mail.utoronto.ca>
2022-11-20 18:28:08 -0500

    added requirement for running the script

--- baseline/run.sh
+++ baseline/run.sh
@@ -1,3 +1,13 @@
+'''
+requirement to run this script:
+
+.local/lib/python3.10/site-packages/torch/optim/optimizer.py
+comment out the line below
+# self.defaults.setdefault('differentiable', False)
+
+
+'''
+
 MODEL_PATH='./checkpoint_wordmap/BEST_checkpoint_coco_5_cap_per_img_5_min_word_freq.pth.tar'
 WORD_MAP_PATH='./checkpoint_wordmap/WORDMAP_coco_5_cap_per_img_5_min_word_freq.json'
 
